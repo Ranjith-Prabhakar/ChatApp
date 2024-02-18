41a1a3ca93a4375e3f45b8e16b7168a6190f6c6a
Ranjith <ranjithp2294@gmail.com>
2024-02-18 21:55:19 +0530

    styling has been completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 // import Login from "./pages/login/Login"
-import SignUp from "./pages/signup/SignUp"
+import Home from "./pages/home/Home"
+// import SignUp from "./pages/signup/SignUp"
 
 const App = () => {
   return (
     <div className="flex p-4 h-screen justify-center items-center">
 {/* <Login /> */}
-<SignUp />
-
+{/* <SignUp /> */}
+<Home/>
     </div>
   )
 }
