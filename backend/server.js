d55edd92f1d166e3864da8c6569848ff6df2b521
Ranjith <ranjithp2294@gmail.com>
2024-02-18 19:19:55 +0530

    message controller file has been created:

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 import connectToMongoDB from "./db/connectToMongodb.js"
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.routes.js'
 dotenv.config()
 const app = express()
 
+app.use(cookieParser())
 app.use(express.json())
+
 const PORT = process.env.PORT || 3000
 
 
