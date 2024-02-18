1e6fc6c2fb1f07a6b7865c227b0bf16264fd66d4
Ranjith <ranjithp2294@gmail.com>
2024-02-18 18:47:02 +0530

    initial setups has been done

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express"
+import dotenv from "dotenv"
+import authRoutes from './routes/auth.route.js'
+import connectToMongoDB from "./db/connectToMongodb.js"
+dotenv.config()
+const app = express()
+
+
+const PORT = process.env.PORT || 3000
+
+
+app.use("/api/auth", authRoutes);
+app.listen(PORT, () => { console.log(`server has been conected on http://localhost:${PORT}`); connectToMongoDB()})
